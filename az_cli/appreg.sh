7b683a9eeed23a91fbfbf097ec22aea0bb76a1cd
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-28 15:48:08 +0100

    add new uri

--- az_cli/appreg.sh
+++ az_cli/appreg.sh
@@ -1,14 +1,13 @@
 # Log into Azure
-# az login
+az login
 
-# Register the app and get the appId
-app_id=$(az ad app create --display-name "EmployeeAPIApp" --identifier-uris "https://employeeapiapp" --query appId -o tsv)
+# Register the app and get the appId using the default tenant domain
+app_id=$(az ad app create --display-name "EmployeeAPIApp" --identifier-uris "https://0fa8fe39-3a85-4ac4-929d-caf83acfd064.onmicrosoft.com/employeeapiapp" --query appId -o tsv)
 
 # Create the client secret
 client_secret=$(az ad app credential reset --id $app_id --append --query password -o tsv)
 
-
-# Update the app to expose API
+# Update the app to expose the API
 az ad app update \
     --id $app_id \
     --set api.oauth2Permissions='[{ "adminConsentDescription": "Access employee data", "adminConsentDisplayName": "Access Employee API", "id": "unique-guid", "isEnabled": true, "type": "User", "value": "access_employee_data" }]'
