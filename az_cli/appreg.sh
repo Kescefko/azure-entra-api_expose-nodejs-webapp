6c9b0f047fcef15e09242b15814c887577cd1d1f
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-28 12:51:44 +0100

    add logic the create secret and query app id

--- az_cli/appreg.sh
+++ az_cli/appreg.sh
@@ -1,8 +1,14 @@
 # Log into Azure
 # az login
 
-# Register the application
-az ad app create --display-name "EmployeeAPIApp" --identifier-uris "https://employeeapiapp" --password "your-secret"
+# Register the app and get the appId
+app_id=$(az ad app create --display-name "EmployeeAPIApp" --identifier-uris "https://employeeapiapp" --query appId -o tsv)
+
+# Create the client secret
+client_secret=$(az ad app credential reset --id $app_id --append --query password -o tsv)
+
 
 # Update the app to expose API
-az ad app update --id <app-id> --set api.oauth2Permissions='[{ "adminConsentDescription": "Access employee data", "adminConsentDisplayName": "Access Employee API", "id": "unique-guid", "isEnabled": true, "type": "User", "value": "access_employee_data" }]'
+az ad app update \
+    --id $app_id \
+    --set api.oauth2Permissions='[{ "adminConsentDescription": "Access employee data", "adminConsentDisplayName": "Access Employee API", "id": "unique-guid", "isEnabled": true, "type": "User", "value": "access_employee_data" }]'
